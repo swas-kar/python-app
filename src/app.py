6528451b6dbbdc6cec83e86a3d9cf67b18f43d5f
SwasK <swas@gmail.com>
2025-05-26 10:58:03 +0000

    Corrects ci github workflow

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message':'huh!'
+        'message':'huh! dunno what to say here',
     })
 
 @app.route('/api/v1/healthz')
