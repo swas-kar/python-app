8fb9574d8e609b17a7c529d073692daac4b0cb96
SwasK <swas@gmail.com>
2025-05-27 07:48:28 +0000

    Modifies values file on CD Step to contain the commit ID tag

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message':'changing this message!!',
+        'message':'You are doing great!',
     })
 
 @app.route('/api/v1/healthz')
