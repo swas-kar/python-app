b55ee1ccd2c6d577b595d045b6686c7027ad443e
SwasK <swas@gmail.com>
2025-05-27 07:00:45 +0000

    adds cd job

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message':'Hello, World from Me!',
+        'message':'Hello, World from Me!!!',
     })
 
 @app.route('/api/v1/healthz')
