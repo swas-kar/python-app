7486fbdedf14271d939d6939fcdb12030de780b9
SwasK <swas@gmail.com>
2025-05-27 08:00:03 +0000

    Modifies $ commit ID tag

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message':'You are doing great!',
+        'message':'This is a cool app!',
     })
 
 @app.route('/api/v1/healthz')
