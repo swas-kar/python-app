4346ed3fc1cd403740638dd5b2faaccc39d4bb89
SwasK <swas@gmail.com>
2025-05-27 07:17:08 +0000

    testing the cicid actions

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message':'Hello, World from Me!!',
+        'message':'changing this message!!',
     })
 
 @app.route('/api/v1/healthz')
